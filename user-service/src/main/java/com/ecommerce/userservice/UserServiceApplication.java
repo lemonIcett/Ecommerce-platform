6c48b276671a1faf091e0937ccfb31a9bfa691e8
unknown <mahasubramanyam0@gmail.com>
2026-05-17 22:52:43 +0530

    feat: add Dockerfiles, docker-compose, Prometheus config and fix user-service security

--- user-service/src/main/java/com/ecommerce/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/ecommerce/userservice/UserServiceApplication.java
@@ -2,12 +2,17 @@ package com.ecommerce.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    SecurityAutoConfiguration.class,
+    SecurityFilterAutoConfiguration.class,
+    ManagementWebSecurityAutoConfiguration.class
+})
 public class UserServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
